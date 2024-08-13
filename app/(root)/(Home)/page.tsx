2ed4507a6a5eb3ae472b1713e6bb5f74ac559962
Jayant <jayant@errsole.com>
2024-08-13 15:20:20 +0530

    corrected TS types

--- app/(root)/(Home)/page.tsx
+++ app/(root)/(Home)/page.tsx
@@ -2,6 +2,7 @@ import Products from "@/components/Home/Products";
 import getData from "@/lib/actions/getData.action";
 import React, { Suspense } from "react";
 import Loading from "@/components/shared/Loading";
+import { Product } from "@/types";
 
 const Home = async () => {
 	return (
@@ -13,9 +14,10 @@ const Home = async () => {
 
 const HomeContent = async () => {
 	const data = await getData();
+	const res: Product[] = JSON.parse(data);
 	return (
 		<section className="px-4 sm:px-6 lg:px-8 py-8">
-			<Products products={data} />
+			<Products products={res} />
 		</section>
 	);
 };
